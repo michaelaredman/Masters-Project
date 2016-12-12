5abee9bf2c9b67da5734251bfdfa38061ab9f7ca
Michael Redman <michaelallanredman@gmail.com>
2016-12-12 11:06:18 +0000

    Rearrage the files and update the model (not working yet)

---
+++ scripts/createData.R
@@ -0,0 +1,27 @@
+setwd("~/4th Year/project/data/csv/")
+load("~/4th Year/project/data/rdata/expected_data.Rda")
+load("~/4th Year/project/data/rdata/simulated_data.Rda")
+load("~/4th Year/project/data/rdata/unusual_areas.Rda")
+asthma <- asthma_expected_i[asthma_expected_i$CCG != '10L',]
+expected = as.vector(asthma['E']$E)
+sim <- Simulated_data[Simulated_data$X != '10L',]
+simulated = as.matrix(sim[,c(2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)])
+rownames(simulated) <- 1:nrow(simulated)
+write.csv(expected, file='expected.csv')
+write.csv(simulated, file='simulated.csv')
+
+library(sp)
+library(shapefiles)
+library(maptools)
+library(rgdal)
+library(spdep)
+setwd("~/4th Year/project/data/shapefiles")
+shape <- readOGR('.', 'CCG_BSC Feb2013  (clipcoast 200m)')
+shape_deathtowales <- shape[shape@data$CCGname != 'Wales',]
+shape_deathtoisleofwight <- shape_deathtowales[shape_deathtowales@data$CCGname != 'NHS Isle of Wight CCG',]
+
+neib <- poly2nb(shape_deathtoisleofwight)
+adj <- nb2mat(neib, style="B") 
+write.csv(adj, file='~/4th Year/project/data/csv/adjacency.csv')
+
+
