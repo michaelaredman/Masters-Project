2b23c9eba21edf19ae010672891abaf8fe21e65d
Michael Redman <michaelallanredman@gmail.com>
2017-01-17 18:21:22 +0000

    Add model to simulate cut mode

---
+++ model/baystdetect.py
@@ -0,0 +1,42 @@
+import pystan
+import pandas as pd
+import numpy as np
+import time
+from matplotlib import pyplot as plt
+
+def load_data():
+    temp_expected = pd.read_csv('../data/csv/expected.csv')
+    E = np.array(temp_expected['x'])
+    
+    temp_sim = pd.read_csv('../data/csv/simulated.csv')
+    temp_times = temp_sim[['Time1', 'Time2', 'Time3', 'Time4', 'Time5', 'Time6', 'Time7', 'Time8', 'Time9', 'Time10', 'Time11', 'Time12', 'Time13', 'Time14', 'Time15']]
+    observed_values = np.matrix(temp_times, dtype=np.int)
+    
+    adj = pd.read_csv('../data/csv/adjacency.csv', index_col=0)
+    W = np.matrix(adj)
+    
+    numRegions = observed_values.shape[0] #number of regions
+    nt = observed_values.shape[1] #number of time points
+    
+    alpha = 0.75 #this was 1 in the model but that makes the covariance matrix singular
+
+    return numRegions, nt, E, W, alpha, observed_values 
+
+numRegions, nt, E, W, alpha, observed_values = load_data()
+
+model_data = {'numRegions': numRegions,
+              'nt': nt,
+              'observed': observed_values,
+              'log_expected': np.log(E),
+              'W_n': int(W.sum()/2.0),
+              'W': W,
+              'alpha': 0.75,
+              'prop_unusual': 0.05}
+
+
+print('Starting fit at: ', time.ctime())
+
+fit = pystan.stan(file='baystdetect.stan', data=model_data, iter=1000, chains=4, init=0)
+
+#fit.plot()
+#plt.show()
---
+++ model/cut model/cut.py
@@ -0,0 +1,112 @@
+import pystan
+import pandas as pd
+import pymc3 as pm
+import numpy as np
+import time
+import seaborn as sns
+import pickle
+import theano
+import theano.tensor as T
+from matplotlib import pyplot as plt
+
+def load_data():
+    temp_expected = pd.read_csv('../../data/csv/expected.csv')
+    E = np.array(temp_expected['x'])
+    
+    temp_sim = pd.read_csv('../../data/csv/simulated.csv')
+    temp_times = temp_sim[['Time1', 'Time2', 'Time3', 'Time4', 'Time5', 'Time6', 'Time7', 'Time8', 'Time9', 'Time10', 'Time11', 'Time12', 'Time13', 'Time14', 'Time15']]
+    observed_values = np.matrix(temp_times, dtype=np.int)
+    
+    adj = pd.read_csv('../../data/csv/adjacency.csv', index_col=0)
+    W = np.matrix(adj)
+    
+    numRegions = observed_values.shape[0] #number of regions
+    nt = observed_values.shape[1] #number of time points
+    
+    alpha = 0.75 #this was 1 in the model but that makes the covariance matrix singular
+
+    return numRegions, nt, E, W, alpha, observed_values 
+
+numRegions, nt, E, W, alpha, observed_values = load_data()
+
+model1_data = {'numRegions': numRegions,
+              'nt': nt,
+              'observed': observed_values,
+              'log_expected': np.log(E),
+              'W_n': int(W.sum()/2.0),
+              'W': W}
+
+def init_chains(nchains):
+    our_list = []
+    for i in range(nchains):
+        our_dict = {'sigma_l' : 0.1 + i*0.1,
+                    'sigma_temporal' : 0.1 + i*0.1,
+                    'sigma_v' : 0.1 + i*0.1,
+                    'alpha' : 0.5 + i*0.05,
+                    'temporal' : [0.1 for i in range(nt)],
+                    'v' : [0.1 for i in range(numRegions)],
+                    'lmbda' : [0.1 for i in range(numRegions)]}
+        our_list.append(our_dict)
+    return our_list
+
+my_init = init_chains(4)
+
+iter_num = 500
+fit_model1 = pystan.stan(file='model1.stan', data=model1_data, iter=iter_num, chains=4, init=my_init)
+
+model1_trace = fit_model1.extract()
+
+ts = time.localtime()
+file_name_m1 = "trace/model1_{}-{}-{}__{}-{}.pkl".format(ts[2], ts[1], ts[0], ts[3], ts[4])
+
+with open('summary_model1.txt', 'w') as f:
+    print(fit_model1, file=f)
+
+with open(file_name_m1, 'wb') as f:
+    pickle.dump(model1_trace, f)
+
+model2_data = {'numRegions' : numRegions,
+               'nt' : nt,
+               'observed' : observed_values,
+               'log_expected' : np.log(E)}
+
+
+fit_model2 = pystan.stan(file='model2.stan', data=model2_data, iter=iter_num, chains=4)
+
+model2_trace = fit_model2.extract()
+
+ts = time.localtime()
+file_name_m2 = "trace/model1_{}-{}-{}__{}-{}.pkl".format(ts[2], ts[1], ts[0], ts[3], ts[4])
+
+with open('summary_model2.txt', 'w') as f:
+    print(fit_model2, file=f)
+
+with open(file_name_m2, 'wb') as f:
+    pickle.dump(model2_trace, f)
+
+
+mu_general = fit_model1['mu_general']
+mu_specific = fit_model2['mu_specific']
+
+mu_general_av = theano.shared(mu_general.mean(axis=0))
+mu_specific_av = theano.shared(mu_specific.mean(axis=0))
+
+prob_unusual = 15/210
+
+model = pm.Model()
+
+with model:
+
+    mix_component = pm.Bernoulli('mix_component', p=prob_unusual, shape=(numRegions))
+
+    mu_temp = []
+    for i in range(numRegions):
+        mu_temp.append((1.0 - mix_component[i]) * mu_general_av[i,:] + mix_component[i] * mu_specific_av[i,:])
+    mu = T.stack(mu_temp)
+
+    observed = pm.Poisson('observed', mu = mu, observed=observed_values, shape=(numRegions, nt))
+
+with model:
+
+    trace = pm.sample(10000)
+
